aec3f3bd8085de8bf45191241f49f29298f127e0
igor <stognij-igor@yandex.ru>
2014-06-18 13:04:38 +0200

    created resource to return all inventorz
    Added inventorz and itil package. Add ToJSON and returnAllParts methods.
    Will be improving on methods in next commit

---
+++ src/com/youtube/rest/inventory/V1_inventory.java
@@ -0,0 +1,70 @@
+package com.youtube.rest.inventory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.codehaus.jettison.json.JSONArray;
+
+import com.youtube.dao.Oracle308tube;
+import com.youtube.util.ToJSON;
+
+/**
+ * This class is used to manage computer parts inventory.
+ * 
+ * We have improved upon inventory resource in episode 5 of this tutorial series, 
+ * at this point v1/inventory should be deprecated and a date should be set 
+ * for this java class to be deleted.
+ * 
+ * @author 308tube and me :)
+ */
+@Path("/v1/inventory")
+public class V1_inventory {
+
+	
+	/** 
+	 * @return - JSON array string
+	 * @throws Exception
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnAllPcParts() throws Exception {
+
+		PreparedStatement query = null;
+		Connection conn = null;
+		String returnString = null;
+		Response rb = null;	
+
+		try {
+			conn = Oracle308tube.Oracle308tubeConn().getConnection();
+			query = conn.prepareStatement("select * " + "from PC_PARTS");
+
+			ResultSet rs = query.executeQuery();
+
+			ToJSON converter = new ToJSON();
+			JSONArray json = new JSONArray();
+
+			json = converter.toJSONArray(rs);
+			query.close(); //close connection
+
+			returnString = json.toString();
+			rb = Response.ok(returnString).build();
+
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			 if (conn != null) conn.close();
+		}
+
+		return rb;
+	}
+	
+}
--- src/com/youtube/rest/status/V1_status.java
+++ src/com/youtube/rest/status/V1_status.java
@@ -1,7 +1,9 @@
 package com.youtube.rest.status;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.*;
+import java.sql.*;
+import com.youtube.dao.*;
 
 /**
  * This is the root path for our restful api service
@@ -17,7 +19,7 @@ import javax.ws.rs.core.MediaType;
 @Path("/v1/status")
 public class V1_status {
 
-	private static final String api_version= "00.01.00";
+	private static final String api_version= "00.02.00";
 	
 	/**
 	 * This method sits at the root of the api.  It will return the name of this api.
@@ -46,5 +48,42 @@ public class V1_status {
 	public String returnVersion(){
 		return "<p>Version:</p>" + api_version;
 	}
+	
+	@Path("/database")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnDatabaseStatus() throws Exception {
+		
+		PreparedStatement query = null;
+		String myString = null;
+		String returnString = null;
+		Connection conn = null;
+		
+		try {
+			conn = Oracle308tube.Oracle308tubeConn().getConnection();
+			query = conn.prepareStatement("select to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS') DATETIME " + 
+			"from sys.dual");
+			ResultSet rs = query.executeQuery();
+			
+			while (rs.next()){
+				// /*Debug*/ System.out.println(rs.getString("DATETIME"));
+				myString = rs.getString("DATETIME");
+			}
+			query.close();// close connection
+			returnString = "<p>Database Status </p> " + "<p>Database Date/Time return: " + myString + "</p>"; 
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		finally {
+			if (conn != null) conn.close();
+		}
+		
+		return returnString;
+	}
+	
+	
+	
+	
 	
 }
