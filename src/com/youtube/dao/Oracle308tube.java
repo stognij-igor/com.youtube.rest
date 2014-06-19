e0b478b0284e589bcb67fd15abefb4f478733171
igor <stognij-igor@yandex.ru>
2014-06-19 12:23:59 +0200

    improve dao package
    added new java class to hold all sql queries and only allow this package
    to run Oracle queries

--- src/com/youtube/dao/Oracle308tube.java
+++ src/com/youtube/dao/Oracle308tube.java
@@ -1,5 +1,7 @@
 package com.youtube.dao;
 
+import java.sql.Connection;
+
 import javax.naming.*;
 import javax.sql.*;
 
@@ -8,12 +10,24 @@ public class Oracle308tube {
 	private static DataSource Oracle308tube = null;
 	private static Context context = null;
 	
+	
 	public static DataSource Oracle308tubeConn() throws Exception{
-		
+	//вообще из соображения безопасности этот метод должен быть private	
+	
+		/**
+		 * check to see if the database object is already defined...
+		 * if it is, then return the connection, no need to look it up again.
+		 */
 		if (Oracle308tube != null){ return Oracle308tube; }		
 		
 		try {
+			/**
+			 * This only needs to run one time to get the database object.
+			 * context is used to lookup the database object in weblogic
+			 * Oracle308tube will hold the database object
+			 */
 			if (context == null){ context = new InitialContext();}
+			
 			Oracle308tube = (DataSource) context.lookup("308tubeOracle");
 		}
 		catch (Exception e) {
@@ -22,4 +36,26 @@ public class Oracle308tube {
 		
 		return Oracle308tube;
 	}
+	
+	/**
+	 * This method will return the connection to the Oracle 308tube schema
+	 * Note that the scope is protected which means only java class in the
+	 * dao package can use this method.
+	 * 
+	 * @return Connection to 308tube Oracle database.
+	 */
+	protected static Connection oraclePcPartsConnection() {
+		Connection conn = null;
+		try {
+			conn = Oracle308tubeConn().getConnection();
+			return conn;
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	
+	
 }
---
+++ src/com/youtube/dao/Schema308tube.java
@@ -0,0 +1,71 @@
+package com.youtube.dao;
+
+import java.sql.*;
+
+import org.codehaus.jettison.json.JSONArray;
+
+import com.youtube.util.ToJSON;
+
+/**
+ * This java class will hold all the sql queries from episode 5 and onward.
+ * V1_inventory.java and V1_status.java will not use this class for its sql code
+ * since they were created before episode 5.
+ * 
+ * Having all sql/database code in one package makes it easier to maintain and audit
+ * but increase complexity.
+ * 
+ * Note: we also used the extends Oracle308tube on this java class to inherit all
+ * the methods in Oracle308tube.java
+ * 
+ * @author 308tube and me
+ */
+public class Schema308tube extends Oracle308tube{
+
+	/**
+	 * This method will search for a specific brand from the PC_PARTS table.
+	 * By using prepareStatement and the ?, we are protecting against sql injection
+	 * 
+	 * Never add parameter straight into the prepareStatement
+	 * 
+	 * @param brand - product brand
+	 * @return - json array of the results from the database
+	 * @throws Exception
+	 */
+	public JSONArray queryReturnBrandParts(String brand) throws Exception {
+
+		PreparedStatement query = null;
+		Connection conn = null;
+
+		ToJSON converter = new ToJSON();
+		JSONArray json = new JSONArray();
+
+		try {
+			conn = oraclePcPartsConnection();
+			query = conn.prepareStatement("select PC_PARTS_PK, PC_PARTS_TITLE, PC_PARTS_CODE, "
+						+"PC_PARTS_MAKER, PC_PARTS_AVAIL, PC_PARTS_DESC from PC_PARTS " +
+											"where UPPER(PC_PARTS_MAKER) = ? ");
+
+			query.setString(1, brand.toUpperCase()); //protect against sql injection
+			// это типа безопасный запрос именно поэтому выше стоит в конце запроса вопросительный знак ?  см. Part 5 - 15:00
+			ResultSet rs = query.executeQuery();
+
+			json = converter.toJSONArray(rs);
+			query.close(); //close connection
+		}
+		catch(SQLException sqlError) {
+			sqlError.printStackTrace();
+			return json;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return json;
+		}
+		finally {
+			if (conn != null) conn.close();
+		}
+
+		return json;
+	}
+
+	
+}
